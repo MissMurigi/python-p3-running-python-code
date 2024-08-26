43ebee8220551626e707ad39f227c3c39a92e661
victoriamurigi <vickymurigi001@gmail.com>
2024-08-26 08:53:59 +0300

    creating a pipenv virtual environment

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+#print("Hello world!")
+#any line that starts with a hashtag wont be executed byb the iintepretorc
+#print("Hello sun!")
+#print("Hello sky!")
+#print("Hello world!", end=" ")
+#print("Hello sun!", end="!! ")
+#print("Hello sky!", end="!!!/n")
+
+print("Hello World! Pass this test, please.")
